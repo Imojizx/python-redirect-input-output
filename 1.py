725c15be2dd66ed03f52b627f7b11ca0800a2912
= <=>
2024-09-05 10:45:40 +0330

    message

---
+++ 1.py
@@ -0,0 +1,13 @@
+import sys
+fout=open("input.txt",'rt')
+sys.stdin=fout
+while True:
+    numbers=input("~>").strip().split(" ")
+    if numbers[0]=="add":
+        print(int(numbers[1])+int(numbers[2]))
+    if numbers[0]=="mul":
+        print(int(numbers[1])*int(numbers[2]))
+    if numbers[0]=="div":
+        print(int(numbers[1])/int(numbers[2]))
+    if numbers[0]=="min":
+        print(int(numbers[1])-int(numbers[2]))
